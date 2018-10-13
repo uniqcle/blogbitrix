2b04117cbff4f90a754756245c15141d684601eb
Андрей Анучкин <uniqcle@yandex.ru>
2018-10-13 20:02:43 +0600

    Подключаем css и js скрипты

--- local/templates/vog_main/header.php
+++ local/templates/vog_main/header.php
@@ -1,25 +1,46 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
-<!--
-https://w3layouts.com/voguish-a-blogging-category-flat-bootstrap-responsive-web-template/
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
+<?php 
+	/*******************************************************
+	// Импортируем пространство имен
+	********************************************************/
+	use Bitrix\Main\Page\Asset; 
+
+	$APPLICATION->ShowHead(); 
+ ?>
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Voguish a Blogging Category Flat Bootstarp Responsive Website Template | Home :: w3layouts</title>
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Voguish Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<title><?php $APPLICATION->ShowTitle(); ?></title>
+
+
+<?php 
+	/*******************************************************
+	// Подключаем Css
+	********************************************************/
+	Asset::getInstance()->addCss(SITE_TEMPLATE_PATH.'/css/bootstrap.css'); 
+	Asset::getInstance()->addCss(SITE_TEMPLATE_PATH.'/css/style.css'); 
+
+	/*******************************************************
+	// Подключаем JS скрипты
+	********************************************************/
+	CJSCore::Init(['jquery']); //Из самого Битрикс
+	Asset::getInstance()->addJs('/js/responsiveslides.min.js'); 
+
+	Asset::getInstance()->addString('<meta name="viewport" content="width=device-width, initial-scale=1">'); 
+	Asset::getInstance()->addString("<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900' rel='stylesheet' type='text/css'>");
+	Asset::getInstance()->addString("<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,700' rel='stylesheet' type='text/css'>");  
+
+
+ ?>
+
+
+ 
+ 
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,700' rel='stylesheet' type='text/css'>
+
+
+ 
 <script src="js/jquery.min.js"></script>
 <script src="js/responsiveslides.min.js"></script>
 <script>
