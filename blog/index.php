741000a9f992675f5ff6be95e8de08ec855a0077
Андрей Анучкин <uniqcle@yandex.ru>
2018-10-14 19:26:34 +0700

    Пагинация (скопировано)

--- blog/index.php
+++ blog/index.php
@@ -2,9 +2,9 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Блог");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:news",
-	"news",
-	Array(
+	"bitrix:news", 
+	"news", 
+	array(
 		"ADD_ELEMENT_CHAIN" => "N",
 		"ADD_SECTIONS_CHAIN" => "Y",
 		"AJAX_MODE" => "N",
@@ -21,11 +21,17 @@ $APPLICATION->SetTitle("Блог");
 		"DETAIL_ACTIVE_DATE_FORMAT" => "j F Y",
 		"DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
 		"DETAIL_DISPLAY_TOP_PAGER" => "N",
-		"DETAIL_FIELD_CODE" => array("",""),
+		"DETAIL_FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
 		"DETAIL_PAGER_SHOW_ALL" => "Y",
 		"DETAIL_PAGER_TEMPLATE" => "",
 		"DETAIL_PAGER_TITLE" => "Страница",
-		"DETAIL_PROPERTY_CODE" => array("AUTHOR",""),
+		"DETAIL_PROPERTY_CODE" => array(
+			0 => "AUTHOR",
+			1 => "",
+		),
 		"DETAIL_SET_CANONICAL_URL" => "N",
 		"DISPLAY_BOTTOM_PAGER" => "Y",
 		"DISPLAY_DATE" => "Y",
@@ -39,8 +45,19 @@ $APPLICATION->SetTitle("Блог");
 		"IBLOCK_TYPE" => "content",
 		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
 		"LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"LIST_FIELD_CODE" => array("NAME","PREVIEW_TEXT","PREVIEW_PICTURE","DATE_ACTIVE_FROM","SHOW_COUNTER","CREATED_USER_NAME",""),
-		"LIST_PROPERTY_CODE" => array("AUTHOR",""),
+		"LIST_FIELD_CODE" => array(
+			0 => "NAME",
+			1 => "PREVIEW_TEXT",
+			2 => "PREVIEW_PICTURE",
+			3 => "DATE_ACTIVE_FROM",
+			4 => "SHOW_COUNTER",
+			5 => "CREATED_USER_NAME",
+			6 => "",
+		),
+		"LIST_PROPERTY_CODE" => array(
+			0 => "AUTHOR",
+			1 => "",
+		),
 		"MESSAGE_404" => "",
 		"META_DESCRIPTION" => "-",
 		"META_KEYWORDS" => "-",
@@ -50,8 +67,8 @@ $APPLICATION->SetTitle("Блог");
 		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
 		"PAGER_SHOW_ALL" => "N",
 		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
+		"PAGER_TEMPLATE" => "blog_pagination",
+		"PAGER_TITLE" => "",
 		"PREVIEW_TRUNCATE_LEN" => "",
 		"SEF_MODE" => "N",
 		"SET_LAST_MODIFIED" => "N",
@@ -70,6 +87,11 @@ $APPLICATION->SetTitle("Блог");
 		"USE_RSS" => "N",
 		"USE_SEARCH" => "N",
 		"USE_SHARE" => "N",
-		"VARIABLE_ALIASES" => Array("ELEMENT_ID"=>"ELEMENT_ID","SECTION_ID"=>"SECTION_ID")
-	)
+		"COMPONENT_TEMPLATE" => "news",
+		"VARIABLE_ALIASES" => array(
+			"SECTION_ID" => "SECTION_ID",
+			"ELEMENT_ID" => "ELEMENT_ID",
+		)
+	),
+	false
 );?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
